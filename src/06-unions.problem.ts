52a030e1b6472906b3b6680ff8c1db66c45a0d7d
Christopher Patti <feoh@feoh.org>
2026-01-12 20:11:37 -0500

    Yay progress. Done up to ex 7

--- src/06-unions.problem.ts
+++ src/06-unions.problem.ts
@@ -1,3 +1,5 @@
+// type roleType = "admin" | "user" | "super-admin"
+
 interface User {
   id: number;
   firstName: string;
@@ -8,7 +10,7 @@ interface User {
    * - 'user'
    * - 'super-admin'
    */
-  role: string;
+  role: "admin" | "user" | "super-admin";
 }
 
 export const defaultUser: User = {
